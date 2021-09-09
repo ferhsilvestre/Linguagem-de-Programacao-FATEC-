8f93cc4acbdc891dab6a558ce25411442d600124
Fernando Silvestre <fernandohsilvestre@hotmail.com>
2021-09-09 16:46:28 -0300

    ex3 da lista 4 modificado

--- Função 4/ex3.py
+++ Função 4/ex3.py
@@ -1,8 +1,7 @@
 # 3. (Função com retorno com parâmetro) Faça um programa contendo uma função/método que receba um valor de porcentagem de aumento via parâmetro, aplique este aumento a um salário de um funcionário, retorne e apresente o novo salário.
 
 def aumento(x,y):
-    salario_novo = x + (x * (y / 100))
-    print(x, y)
+    salario_novo = x + (x * (y / 100))    
     return salario_novo
 
 def main():
